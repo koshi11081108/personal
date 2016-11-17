2f236e035c06d0c30a3da04dd002be7899fdc32a
kyamamoto <kyamamoto@fourmix.co.jp>
2016-11-17 13:17:11 +0900

    基盤

---
+++ element/logo.php
@@ -0,0 +1,40 @@
+<header>
+	<div id="inner">
+		<h1 id="logo">
+				山本社会保険労務士事務所
+		</h1>
+		<ul id="headermenu">
+			<li><a href="contact.html">お問い合わせはこちら</a></li>
+		</ul>
+		<p id="tel"><strong>TEL:0538-43-1896</strong>（AM9:00〜PM7:00）</p>
+		<ul id="fsize">
+			<li id="me"><a href="#" onclick="setActiveStyleSheet('me'); return false;" title="文字サイズを元に戻す">文字サイズを元に戻す</a></li>
+			<li id="la"><a href="#" onclick="setActiveStyleSheet('la'); return false;" title="文字サイズを大きくする">文字サイズを大きくする</a></li>
+		</ul>
+	</div>
+</header>
+
+<div id="menu-box">
+	<!--PC用（481px以上端末）メニュー-->
+	<nav id="menubar">
+		<ul>
+			<li class="current"><a href="index.php">ホーム</a></li>
+			<li><a href="company.html">事務所概要</a></li>
+			<li><a href="baibai.html">社労士とは</a></li>
+			<li><a href="chintai.html">業務案内</a></li>
+			<li><a href="tenant.html">助成金</a></li>
+			<li><a href="parking.html">相談事例</a></li>
+		</ul>
+	</nav>
+	<!--スマホ用（480px以下端末）メニュー-->
+	<nav id="menubar-s">
+		<ul>
+			<li class="current"><a href="index.php">ホーム</a></li>
+			<li><a href="company.html">事務所概要</a></li>
+			<li><a href="baibai.html">社労士とは</a></li>
+			<li><a href="chintai.html">業務案内</a></li>
+			<li><a href="tenant.html">助成金</a></li>
+			<li><a href="parking.html">相談事例</a></li>
+		</ul>
+	</nav>
+</div>
---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>山本社会保険労務士事務所　静岡県袋井市　就業規則　助成金　給与計算事務の代行　人事労務管理等</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="山本吉己社会保険労務士事務所　静岡県袋井市にて　人事労務問題、就業規則の作成メンテナンス、助成金の受給診断、給与計算事務代行等お気軽にご相談ください。">
+	<meta name="keywords" content="山本社会保険労務士事務所, 山本吉己, 社会保険労務士, 人事労務問題, 給与計算, 就業規則, 助成金, 静岡県, 袋井市、社労士、セクハラ、袋井、">
+	<!-- css,js -->
+	<link rel="stylesheet" href="css/slide.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="alternate stylesheet" href="css/large.css" title="la">
+	<script type="text/javascript" src="js/styleswitcher.js"></script>
+	<script type="text/javascript" src="js/openclose.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+</head>
+
+<body>
+
+	<?php include(dirname(__FILE__).'/element/logo.php'); ?>
+
+	<div id="contents">
+
+		<div id="main">
+
+			<aside id="mainimg">
+				<img src="images/1.jpg" alt="" id="slide1"/>
+				<img src="images/2.jpg" alt="" id="slide2"/>
+				<img src="images/3.jpg" alt="" id="slide3"/>
+			</aside>
+
+			<section id="new">
+				<h2 id="newinfo_hdr" class="close">更新情報・お知らせ</h2>
+				<dl id="newinfo">
+					<dt>2016/09/XX</dt>
+					<dd>tp_home1_fudosan公開。<span class="newicon">NEW</span></dd>
+					<dt>20XX/00/00</dt>
+					<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+					<dt>20XX/00/00</dt>
+					<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+					<dt>20XX/00/00</dt>
+					<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+					<dt>20XX/00/00</dt>
+					<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+					<dt>20XX/00/00</dt>
+					<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+					<dt>20XX/00/00</dt>
+					<dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+				</dl>
+			</section>
+
+			<section>
+
+				<h2>テンプレートのご利用前に必ずお読み下さい</h2>
+
+				<h3>利用規約のご案内</h3>
+				<p>このテンプレートは、<a href="http://template-party.com/">Template Party</a>にて無料配布している『不動産業者向け無料ホームページテンプレート tp_home1_fudosan』です。必ずダウンロード先のサイトの<a href="http://template-party.com/read.html">利用規約</a>をご一読の上でご利用下さい。</p>
+				<p><span class="color1">■<strong>HP最下部の著作表示『Web Design:Template-Party』は無断で削除しないで下さい。</strong></span><br>
+					わざと見えなく加工する事も禁止です。</p>
+					<p><span class="color1">■<strong>下部の著作を外したい場合は</strong></span><br>
+						<a href="http://template-party.com/">Template-Party</a>の<a href="http://template-party.com/member.html">ライセンス契約</a>を行う事でHP下部の著作を外す事ができます。</p>
+
+						<h3>メニューなどの開閉ブロック（openclose.js）について</h3>
+						<p>当テンプレートの開閉ブロックパーツは<a href="http://www.crytus.co.jp/">有限会社クリタス様</a>提供のプログラムを使用しています。openclose.jsファイルは改変せずにご利用下さい。<br>
+							また、当サイトのテンプレート以外に使いたいなど、「プログラムのみ」を使う場合は<a href="http://template-party.com/free_program/openclose_license.html">こちらの規約</a>をお守り下さい。</p>
+
+							<h3>当テンプレートの詳しい使い方は</h3>
+							<p><a href="company.html#about">こちらをご覧下さい。</a></p>
+
+						</section>
+
+						<section>
+							<h2>不動産サイト用プログラム販売中</h2>
+							<p>当テンプレートの物件紹介ページにも使えるCMSを販売中。物件一覧ページ以外に、各詳細ページや専用お問い合わせフォームも出力できます。<a href="http://template-party.com/file/cms_fudosan.html">詳しくはこちらをご覧下さい。</a><br>
+								<a href="http://template-party.com/file/cms_fudosan.html"><img src="http://template-party.com/images/file/cms_fudosan/img_temp600.jpg" alt="" width="600" height="252" class="wa"></a></p>
+							</section>
+
+						</div>
+						<!--/main-->
+
+						<div id="sub">
+
+							<nav class="box1">
+								<h2>SUB MENU</h2>
+								<ul class="submenu mb10">
+									<li><a href="#">主要リンクサンプル</a></li>
+									<li><a href="#">主要リンクサンプル</a></li>
+									<li><a href="#">主要リンクサンプル</a></li>
+								</ul>
+							</nav>
+
+							<aside>
+								<h2>関連情報</h2>
+								<ul class="submenu">
+									<li><a href="#">関連情報リンク</a></li>
+									<li><a href="#">関連情報リンク</a></li>
+									<li><a href="#">関連情報リンク</a></li>
+								</ul>
+							</aside>
+
+							<div class="box1">
+								<section>
+									<h2>この見出しはh2タグです</h2>
+									<p>このボックスは、class="box1"と指定すれば出ます。</p>
+								</section>
+							</div>
+							<!--/box1-->
+
+							<section>
+								<h2>この見出しはh2タグです</h2>
+								<p>ここのスペースも使えます。</p>
+							</section>
+
+						</div>
+						<!--/sub-->
+
+						<p id="pagetop"><a href="#">↑ </a></p>
+
+					</div>
+					<!--/contents-->
+
+					<footer>
+
+						<div id="footermenu">
+							<ul>
+								<li><a href="index.php">ホーム</a></li>
+								<li><a href="company.html">会社概要</a></li>
+								<li><a href="contact.html">お問い合わせ</a></li>
+							</ul>
+							<ul>
+								<li><a href="baibai.html">売買物件</a></li>
+								<li><a href="chintai.html">賃貸物件</a></li>
+								<li><a href="tenant.html">店舗物件</a></li>
+								<li><a href="parking.html">駐車場</a></li>
+							</ul>
+							<ul>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+							</ul>
+							<ul>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+							</ul>
+							<ul>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+								<li><a href="#">メニューサンプル</a></li>
+							</ul>
+						</div>
+						<!--/footermenu-->
+
+						<div id="copyright">
+							<small>Copyright&copy; <a href="index.php">山本社会保険労務士事務所.</a> All Rights Reserved.</small>
+							<span class="pr"><a href="http://template-party.com/" target="_blank">※当サイトのテキスト・画像等すべての転載転用、商用販売を固く禁じます。</a></span>
+						</div>
+
+					</footer>
+
+					<!--スマホ用更新情報　480px以下-->
+					<script type="text/javascript">
+						if (OCwindowWidth() <= 480) {
+							open_close("newinfo_hdr", "newinfo");
+						}
+					</script>
+
+					<!--メニューの３本バー-->
+					<div id="menubar_hdr" class="close"><span></span><span></span><span></span></div>
+					<!--メニューの開閉処理条件設定　480px以下-->
+					<script type="text/javascript">
+						if (OCwindowWidth() <= 480) {
+							open_close("menubar_hdr", "menubar-s");
+						}
+					</script>
+				</body>
+				</html>
