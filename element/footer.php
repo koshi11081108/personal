810bebd57f2d531440d5a40ee93a275bfab1665f
kyamamoto <kyamamoto@fourmix.co.jp>
2016-11-18 18:10:55 +0900

    エレメント化

---
+++ element/footer.php
@@ -0,0 +1,27 @@
+<footer>
+
+	<div id="footermenu">
+	</div>
+
+	<div id="copyright">
+		<small>Copyright&copy; <a href="index.php">山本社会保険労務士事務所.</a> All Rights Reserved.</small>
+		<span class="pr"><a href="http://template-party.com/" target="_blank">※当サイトのテキスト・画像等すべての転載転用、商用販売を固く禁じます。</a></span>
+	</div>
+
+</footer>
+
+<!--スマホ用更新情報　480px以下-->
+<script type="text/javascript">
+	if (OCwindowWidth() <= 480) {
+		open_close("newinfo_hdr", "newinfo");
+	}
+</script>
+
+<!--メニューの３本バー-->
+<div id="menubar_hdr" class="close"><span></span><span></span><span></span></div>
+<!--メニューの開閉処理条件設定　480px以下-->
+<script type="text/javascript">
+	if (OCwindowWidth() <= 480) {
+		open_close("menubar_hdr", "menubar-s");
+	}
+</script>
---
+++ element/head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>山本社会保険労務士事務所　静岡県袋井市　就業規則　助成金　給与計算事務の代行　人事労務管理等</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="山本吉己社会保険労務士事務所　静岡県袋井市にて　人事労務問題、就業規則の作成メンテナンス、助成金の受給診断、給与計算事務代行等お気軽にご相談ください。">
+	<meta name="keywords" content="山本社会保険労務士事務所, 山本吉己, 社会保険労務士, 人事労務問題, 給与計算, 就業規則, 助成金, 静岡県, 袋井市、社労士、セクハラ、袋井、">
+	<!-- css,js -->
+	<link rel="stylesheet" href="css/slide.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="alternate stylesheet" href="css/large.css" title="la">
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/styleswitcher.js"></script>
+	<script type="text/javascript" src="js/openclose.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+	<!--  -->
+	<link rel="alternate stylesheet" href="css/custom.css">
+	<script type="text/javascript" src="js/loadAnimation.js"></script>
+	<?php include(dirname(__FILE__).'php/visitor.php') ?>
+</head>
